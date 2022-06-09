5fc3ae189cf389c8751038968363580f8297249d
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2022-06-09 16:13:45 +0300

    Initial commit

---
+++ src/by/itstep/elena/megachopproject/model/entity/Orange.java
@@ -0,0 +1,45 @@
+package by.itstep.elena.megachopproject.model.entity;
+
+public class Orange {
+    private int diameter; //поля (состояние)
+    private int vitaminC;
+    String name;
+
+    public Orange() { //дефолтный конструктор. Должен быть всегда в entity классах
+
+    }
+
+    public Orange(int diameter, int vitaminC, double price) { //конструктор с параметрами, инициализировать состояние объекта
+        super(price);
+        this.diameter = diameter;
+        this.vitaminC = vitaminC;
+    }
+    public int getDiameter() {
+        return diameter;
+    }
+
+    public void setDiameter(int diameter) {
+        this.diameter = diameter;
+    }
+
+    public int getVitaminC() {
+        return vitaminC;
+    }
+
+    public void setVitaminC(int vitaminC) {
+        this.vitaminC = vitaminC;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+
+
+
+
+}
