d61d1434e7e5708f36bb9d48b228a0ec93040f2d
ElenaShylionak <99590421+ElenaShylionak@users.noreply.github.com>
2022-06-10 21:41:26 +0300

    Add Abstract class BasketSorter

---
+++ src/by/itstep/elena/megachopproject/model/logic/BasketSorter.java
@@ -0,0 +1,6 @@
+package by.itstep.elena.megachopproject.model.logic;
+
+public class BasketSorter { //будем делать сортировку по убыванию и возрастанию
+    // нужно реализовать обычную сортировку
+
+}
